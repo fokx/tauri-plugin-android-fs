a56ffca6537ba3b1ef2860bdc0768e7c63e646c5
OkaYu <aiueo13>
2025-02-16 14:21:46 +0900

    Modify indent

--- src/error.rs
+++ src/error.rs
@@ -6,53 +6,53 @@ pub type Result<T> = std::result::Result<T, crate::Error>;
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, thiserror::Error)]
 pub enum PathError {
 
-	/// When the path contains consecutive separators.
-	#[error("The path contains consecutive separators.")]
+    /// When the path contains consecutive separators.
+    #[error("The path contains consecutive separators.")]
     ConsecutiveSeparator,
 
-	/// When the path does not contain a filename.
-	#[error("The path does not contain a filename.")]
+    /// When the path does not contain a filename.
+    #[error("The path does not contain a filename.")]
     DoesNotContainFileName,
 
-	/// When the path does not contain a subdirectory.
-	#[error("The path does not contain a subdirectory.")]
+    /// When the path does not contain a subdirectory.
+    #[error("The path does not contain a subdirectory.")]
     DoesNotContainSubDir,
 
-	/// When the path is empty.
-	#[error("The path is empty.")]
-	Empty,
+    /// When the path is empty.
+    #[error("The path is empty.")]
+    Empty,
 }
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
 
-	#[error("This device is not running Android. This plugin is only supported on Android.")]
-	NotAndroid,
+    #[error("This device is not running Android. This plugin is only supported on Android.")]
+    NotAndroid,
 
-	#[error(transparent)]
-	Path(#[from] PathError),
+    #[error(transparent)]
+    Path(#[from] PathError),
 
-	#[error(transparent)]
-	Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
   
-	#[error(transparent)]
-	PluginInvoke(#[from] anyhow::Error),
+    #[error(transparent)]
+    PluginInvoke(#[from] anyhow::Error),
 }
 
 #[cfg(target_os = "android")]
 impl From<tauri::plugin::mobile::PluginInvokeError> for crate::Error {
 
-	fn from(value: tauri::plugin::mobile::PluginInvokeError) -> Self {
-		Self::PluginInvoke(anyhow::anyhow!("{value}"))
-	}
+    fn from(value: tauri::plugin::mobile::PluginInvokeError) -> Self {
+        Self::PluginInvoke(anyhow::anyhow!("{value}"))
+    }
 }
 
 impl Serialize for crate::Error {
 
-	fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-	where
-		S: Serializer,
-	{
-		serializer.serialize_str(self.to_string().as_ref())
-  	}
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
 }
--- src/models.rs
+++ src/models.rs
@@ -5,53 +5,53 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Deserialize, Serialize)]
 pub enum VisualMediaTarget {
 
-	/// Allow only images to be selected.  
-	ImageOnly,
+    /// Allow only images to be selected.  
+    ImageOnly,
 
-	/// Allow only videos to be selected.  
-	VideoOnly,
+    /// Allow only videos to be selected.  
+    VideoOnly,
 
-	/// Allow only images and videos to be selected.  
-	ImageAndVideo
+    /// Allow only images and videos to be selected.  
+    ImageAndVideo
 }
 
 /// The application specific directory.  
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Deserialize, Serialize)]
 pub enum PrivateDir {
 
-	/// The application specific persistent-data directory.  
-	/// 
-	/// The system prevents other apps from accessing these locations, and on Android 10 (API level 29) and higher, these locations are encrypted.  
-	///  
-	/// These files will be deleted when the app is uninstalled and may also be deleted at the user’s request.  
-	/// 
-	/// ex: `/data/user/0/{app-package-name}/files`
-	Data,
+    /// The application specific persistent-data directory.  
+    /// 
+    /// The system prevents other apps from accessing these locations, and on Android 10 (API level 29) and higher, these locations are encrypted.  
+    ///  
+    /// These files will be deleted when the app is uninstalled and may also be deleted at the user’s request.  
+    /// 
+    /// ex: `/data/user/0/{app-package-name}/files`
+    Data,
 
-	/// The application specific cache directory.  
-	/// 
-	/// The system prevents other apps from accessing these locations, and on Android 10 (API level 29) and higher, these locations are encrypted.  
-	/// 
-	/// These files will be deleted when the app is uninstalled and may also be deleted at the user’s request. 
-	/// In addition, the system will automatically delete files in this directory as disk space is needed elsewhere on the device.  
-	/// 
-	/// ex: `/data/user/0/{app-package-name}/cache`
-	Cache,
+    /// The application specific cache directory.  
+    /// 
+    /// The system prevents other apps from accessing these locations, and on Android 10 (API level 29) and higher, these locations are encrypted.  
+    /// 
+    /// These files will be deleted when the app is uninstalled and may also be deleted at the user’s request. 
+    /// In addition, the system will automatically delete files in this directory as disk space is needed elsewhere on the device.  
+    /// 
+    /// ex: `/data/user/0/{app-package-name}/cache`
+    Cache,
 }
 
 /// Directory in which to place images that are available to other applications and users.  
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Deserialize, Serialize)]
 pub enum PublicImageDir {
 
-	/// Standard directory in which to place pictures that are available to the user.  
-	/// 
-	/// ex: `~/Pictures`
-	Pictures,
+    /// Standard directory in which to place pictures that are available to the user.  
+    /// 
+    /// ex: `~/Pictures`
+    Pictures,
 
-	/// The traditional location for pictures and videos when mounting the device as a camera.  
-	/// 
-	/// ex: `~/DCIM`
-	DCIM,
+    /// The traditional location for pictures and videos when mounting the device as a camera.  
+    /// 
+    /// ex: `~/DCIM`
+    DCIM,
 }
 
 /// Directory in which to place videos that are available to other applications and users.  
@@ -73,75 +73,74 @@ pub enum PublicVideoDir {
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Deserialize, Serialize)]
 pub enum PublicAudioDir {
 
-	/// Standard directory in which to place movies that are available to the user.  
-	/// 
-	/// ex: `~/Music`
-	Music,
+    /// Standard directory in which to place movies that are available to the user.  
+    /// 
+    /// ex: `~/Music`
+    Music,
 
-	/// Standard directory in which to place any audio files that should be in the list of alarms that the user can select (not as regular music).  
-	/// 
-	/// ex: `~/Alarms`
-	Alarms,
+    /// Standard directory in which to place any audio files that should be in the list of alarms that the user can select (not as regular music).  
+    /// 
+    /// ex: `~/Alarms`
+    Alarms,
 
-	/// Standard directory in which to place any audio files that should be in the list of audiobooks that the user can select (not as regular music).  
-	/// 
-	/// This is not available on Android 9 (API level 28) and lower.  
-	/// Availability on a given device can be verified by calling [`PublicStorage::is_audiobooks_dir_available`](crate::PublicStorage::is_audiobooks_dir_available).  
-	/// 
-	/// ex: `~/Audiobooks`  
-	Audiobooks,
+    /// Standard directory in which to place any audio files that should be in the list of audiobooks that the user can select (not as regular music).  
+    /// 
+    /// This is not available on Android 9 (API level 28) and lower.  
+    /// Availability on a given device can be verified by calling [`PublicStorage::is_audiobooks_dir_available`](crate::PublicStorage::is_audiobooks_dir_available).  
+    /// 
+    /// ex: `~/Audiobooks`  
+    Audiobooks,
 
-	/// Standard directory in which to place any audio files that should be in the list of notifications that the user can select (not as regular music).  
-	/// 
-	/// ex: `~/Notifications`
-	Notifications,
+    /// Standard directory in which to place any audio files that should be in the list of notifications that the user can select (not as regular music).  
+    /// 
+    /// ex: `~/Notifications`
+    Notifications,
 
-	/// Standard directory in which to place any audio files that should be in the list of podcasts that the user can select (not as regular music).  
-	/// 
-	/// ex: `~/Podcasts`
-	Podcasts,
+    /// Standard directory in which to place any audio files that should be in the list of podcasts that the user can select (not as regular music).  
+    /// 
+    /// ex: `~/Podcasts`
+    Podcasts,
 
-	/// Standard directory in which to place any audio files that should be in the list of ringtones that the user can select (not as regular music).  
-	/// 
-	/// ex: `~/Ringtones`
-	Ringtones,
+    /// Standard directory in which to place any audio files that should be in the list of ringtones that the user can select (not as regular music).  
+    /// 
+    /// ex: `~/Ringtones`
+    Ringtones,
 
-	/// Standard directory in which to place any audio files that should be in the list of voice recordings recorded by voice recorder apps that the user can select (not as regular music).   
-	/// 
-	/// This is not available on Android 11 (API level 30) and lower.  
-	/// Availability on a given device can be verified by calling [`PublicStorage::is_recordings_dir_available`](crate::PublicStorage::is_recordings_dir_available).  
-	/// 
-	/// ex: `~/Recordings`
-	Recordings,
+    /// Standard directory in which to place any audio files that should be in the list of voice recordings recorded by voice recorder apps that the user can select (not as regular music).   
+    /// 
+    /// This is not available on Android 11 (API level 30) and lower.  
+    /// Availability on a given device can be verified by calling [`PublicStorage::is_recordings_dir_available`](crate::PublicStorage::is_recordings_dir_available).  
+    /// 
+    /// ex: `~/Recordings`
+    Recordings,
 }
 
 /// Directory in which to place files that are available to other applications and users.  
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Deserialize, Serialize)]
 pub enum PublicGeneralPurposeDir {
 
-	/// Standard directory in which to place documents that have been created by the user.  
-	/// 
-	/// ex: `~/Documents`
-	Documents,
+    /// Standard directory in which to place documents that have been created by the user.  
+    /// 
+    Documents,
 
-	/// Standard directory in which to place files that have been downloaded by the user.  
-	/// 
-	/// ex: `~/Download`
-	Download,
+    /// Standard directory in which to place files that have been downloaded by the user.  
+    /// 
+    /// ex: `~/Download`
+    Download,
 }
 
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Deserialize, Serialize)]
 pub(crate) enum PublicDir {
-	#[serde(untagged)]
-	Image(PublicImageDir),
+    #[serde(untagged)]
+    Image(PublicImageDir),
 
-	#[serde(untagged)]
-	Video(PublicVideoDir),
+    #[serde(untagged)]
+    Video(PublicVideoDir),
 
-	#[serde(untagged)]
-	Audio(PublicAudioDir),
+    #[serde(untagged)]
+    Audio(PublicAudioDir),
 
-	#[serde(untagged)]
-	GeneralPurpose(PublicGeneralPurposeDir),
+    #[serde(untagged)]
+    GeneralPurpose(PublicGeneralPurposeDir),
 }
